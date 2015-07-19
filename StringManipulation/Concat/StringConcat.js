f79403bbc3cd87ddbedb30bf953a2e60df099657
unknown <dchougule@TRAINING7.internal.tavisca.com>
2015-07-19 20:34:00 +0530

    String manipulation first commit

---
+++ StringManipulation/Concat/StringConcat.js
@@ -0,0 +1,10 @@
+﻿String.prototype.Concat = function (string2) {
+    var string1 = this;
+   
+    var string3 = string1;
+    for (i = 0; i < string2.length; i++) {
+        string3 += string2.charAt(i);
+    }
+ return string3;
+
+};
---
+++ StringManipulation/SubString/SubString.js
@@ -0,0 +1,23 @@
+﻿String.prototype.SubString = function (startIndex,endIndex) {
+    var string = this;
+    if (startIndex >= string.length || startIndex < 0)
+    {
+        alert('Invalid Start Index.');
+        return;
+    }
+
+    if (endIndex >= string.length || endIndex < startIndex)
+    {
+        alert('Invalid End Index.');
+        return;
+    }
+    var answer = "";
+
+    for (i = startIndex; i <endIndex; i++)
+    {
+
+        answer += string.charAt(i);
+    }
+
+    return answer;
+};
