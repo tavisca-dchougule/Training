173bd28f05360ac553ec6fb54e558a61b64d3e2f
unknown <dchougule@TRAINING7.internal.tavisca.com>
2015-07-21 10:19:22 +0530

    view added

---
+++ BouncingBall/View/Ball.js
@@ -0,0 +1,23 @@
+﻿window.ball = window.ball || {
+    previousLeft : 0,
+ previousTop : 0,
+ deltaX : 10,
+ deltaY : 10
+
+};
+
+ball.bounce = function ()
+{
+    var element = document.getElementById('circle');
+    element.style.left = ball.previousLeft + "px";
+    element.style.top = ball.previousTop + "px";
+
+    ball.container();
+    ball.previousLeft += ball.deltaX;
+    ball.previousTop += ball.deltaY;
+   
+    
+}
+window.ball.start = function () {
+    setInterval(ball.bounce, 10);
+}
---
+++ BouncingBall/View/Container.js
@@ -0,0 +1,24 @@
+﻿window.ball = window.ball || {}
+
+ball.container = function () {
+
+    var height = (window.innerHeight - 50);
+    var width = (window.innerWidth - 50);
+
+    if (ball.previousTop >= height) {
+        ball.deltaY = -10;
+        return;
+    }
+    if (ball.previousLeft >= width) {
+        ball.deltaX = -10;
+        return;
+    }
+    if (ball.previousTop <= 0) {
+        ball.deltaY = 10;
+        return;
+    }
+    if (ball.previousLeft <= 0) {
+        ball.deltaX = 10;
+        return;
+    }
+}
